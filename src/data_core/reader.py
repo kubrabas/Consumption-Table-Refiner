4fb950583e2b4bfc4b03cdc038f56bac85122507
kubrabas <kubraaabasss@gmail.com>
2025-11-30 23:34:12 +0100

    added new functionalities to the DataReader class

--- src/data_core/reader.py
+++ src/data_core/reader.py
@@ -36,3 +36,32 @@ class DataReader:
             raise ValueError("Data failed to load or the file is empty after reading.")
             
         return self.df
+
+    def clean_dataframe(self):
+        """
+        Drop columns and rows that are completely NaN.
+        """
+        self.df = self.df.dropna(axis=1, how='all')
+        self.df = self.df.dropna(axis=0, how='all')
+
+        return self.df
+    
+
+    def drop_unnamed(self):
+        """
+        If all column names start with 'Unnamed', treat the first row as data,
+        use the second row as the header, and drop the original header row.
+        """
+
+        # Convert column names to strings and check them
+        col_names = [str(c) for c in self.df.columns]
+
+        # If all column names start with 'Unnamed'
+        if all(name.startswith("Unnamed") or name == "nan" for name in col_names):
+        # Use the first row as the new header
+            new_header = self.df.iloc[0]   # row 0
+            self.df = self.df[1:]          # drop the first row
+            self.df.columns = new_header   # set new column names
+            self.df = self.df.reset_index(drop=True)  # reset the index
+            return self.df
+
