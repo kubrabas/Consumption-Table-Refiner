ef3b106dd01bba57968a9e275270dbf115921788
kubrabas <kubraaabasss@gmail.com>
2025-12-07 17:42:21 +0100

    Add TimeColumnDetector with name-based detection of time-related columns

--- src/intelligence/columns/__init__.py
+++ src/intelligence/columns/__init__.py
@@ -1,7 +1,9 @@
 from .base import BaseColumnDetector
 from .consumption import ConsumptionColumnDetector
+from .time import TimeColumnDetector
 
 __all__ = [
     "BaseColumnDetector",
     "ConsumptionColumnDetector",
+    "TimeColumnDetector"
 ]
